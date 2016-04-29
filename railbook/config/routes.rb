0c1bac1fb337c9d77be68ca7cdd593fc01cb684f
tomoharun1974 <tomoharu21@gmail.com>
2016-04-29 21:19:30 +0900

    hello world add

--- railbook/config/routes.rb
+++ railbook/config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  # add url pattern 20160429
+  match ':controller(/:action(/:id))',via:[ :get, :post, :patch]
+
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
