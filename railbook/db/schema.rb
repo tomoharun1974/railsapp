889336b6cf913fc55dca592da436b86da65ba614
Tomoharu Nishio <tomoharunishio@Tomoharu-no-MacBook-Pro.local>
2016-05-04 11:08:44 +0900

    scaffold test

--- railbook/db/schema.rb
+++ railbook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503014741) do
+ActiveRecord::Schema.define(version: 20160504013840) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn",       limit: 255
