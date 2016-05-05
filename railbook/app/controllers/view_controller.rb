5f65b099c5bfa91701480a556b2f87ca19c82767
Tomoharu Nishio <tomoharunishio@Tomoharu-no-MacBook-Pro.local>
2016-05-05 21:39:16 +0900

    view select add

--- railbook/app/controllers/view_controller.rb
+++ railbook/app/controllers/view_controller.rb
@@ -4,9 +4,13 @@ class ViewController < ApplicationController
  def index
   render text: 'select'
  end
-
+ def form_for
+  @book = Book.new
+ end
  def select
   @book = Book.new(publish: '技術評論社')
   @books = Book.select(:id,:publish).distinct
+  @review = Review.new
+  @authors = Author.all
  end
 end
